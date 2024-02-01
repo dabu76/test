a068ae2f0a30fe00f1e653706c63b3b31fef22a5
dabu76 <whghtjd45@gmail.com>
2024-02-01 18:09:24 +0900

    feet: 회원가입

---
+++ src/main/java/com/hanulso/Hanulsoshop1Application.java
@@ -0,0 +1,13 @@
+package com.hanulso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hanulsoshop1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hanulsoshop1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanulso/Hanulsoshop1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanulso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hanulsoshop1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
